1fdaf21148bcb85faae488bd6de2e7b2d59b8d8c
Atharva Waranashiwar <waranashiwaratharva@gmail.com>
2024-10-09 17:01:38 -0400

    Test config change

--- tests/test_settings.py
+++ tests/test_settings.py
@@ -1,2 +1,2 @@
 # test_settings.py
-TEST_DATABASE_URL = "postgresql+asyncpg://test_username:test_password@localhost:5432/test_db"
+TEST_DATABASE_URL = "postgresql+asyncpg://test_user:test_password@localhost:5432/test_db"
